3d32df27c605f9c0160347229f4b434d5738fb43
SanaIshaqat <sanaishaqat@gmail.com>
2021-09-21 23:06:16 +0300

    lab13

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ import {
   Route
 } from "react-router-dom";
 import BestBooks from './BestBooks';
+import axios from 'axios';
+import { Form, Button } from "react-bootstrap";
 
 
 class App extends React.Component {
@@ -16,41 +18,158 @@ class App extends React.Component {
     super(props);
     this.state = {
       user: null,
+      books: [],
+      showUpdate: false,
+      id: " ",
+      title: " ",
+      description: " ",
+      status: " ",
+      email: " ",
+
+
     }
   }
 
-  loginHandler = (user) => {
+  componentDidMount = async () => {
+    axios.get(`${process.env.REACT_APP_BACKEND_SERVER}/get-data`).then(response => {
+      this.setState({
+        books: response.data
+      });
+      console.log(response.data);
+    })
+
+  };
+
+  handleDelete = (id) => {
+    let config = {
+      method: "DELETE",
+      baseURL: process.env.REACT_APP_BACKEND_SERVER,
+      url: `/delete-book/${id}`
+    }
+    axios(config).then(res => {
+      console.log(res.data);
+    });
+  }
+
+
+  handleTitle = (e) => {
+    this.setState({ title:e.target.value });
+  }
+  handleDescription = (e) => {
+    this.setState({ description:e.target.value });
+  }
+  handleStatus = (e) => {
+    this.setState({ status:e.target.value });
+  }
+  handleEmail = (e) => {
+    this.setState({ email:e.target.value });
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    let config = {
+      method: "POST",
+      baseURL: process.env.REACT_APP_BACKEND_SERVER,
+      url: `/create-book`,
+      data: {
+        title: this.state.title,
+        description: this.state.description,
+        status: this.state.status,
+        email: this.state.email
+      }
+    }
+    axios(config).then(res => {
+      console.log(res.data)
+      this.setState({
+        books: res.data
+      })
+    })
+  }
+  handleUpdate=(id,title,description,status,email,)=>{
     this.setState({
-      user,
+      title:title,
+      description:description,
+      status:status,
+      email:email,
+      id:id,
+      showUpdate:true
     })
   }
 
-  logoutHandler = () => {
-    this.setState({
-      user: null,
-    })
+  handleUpdateForm=()=>{
+    let config={
+      method:"PUT",
+      baseURL:process.env.REACT_APP_BACKEND_URL,
+      url:`/update-data/${this.state.id}`,
+      data:{
+        title:this.state.title,
+        description:this.state.description,
+        status:this.state.status,
+        email:this.state.email
+      }
+    }
+    axios(config).then(res=>{
+      this.setState({
+        books:res.data
+      })
+    });
   }
+
+
+
+
 
   render() {
     return (
       <>
-     
-         <Router>
+
+        <Router>
           <Header user={this.state.user} onLogout={this.logoutHandler} />
-          <Switch> 
-             <Route exact path="//get-data"> 
-               {/* TODO: if the user is logged in, render the `BestBooks` component, if they are not, render the `Login` component */}
-               
-        
+          <Switch>
+            <Route exact path="//get-data">
+             /* TODO: if the user is logged in, render the `BestBooks` component, if they are not, render the `Login` component */
             </Route>
-             {/* TODO: add a route with a path of '/profile' that renders a `Profile` component */} 
-           </Switch>
-           <BestBooks/> 
+             /* TODO: add a route with a path of '/profile' that renders a `Profile` component */
+          </Switch>
+          <br />
+          
+          <Form onSubmit={this.handleSubmit}>
+            <Form.Group className="mb-3" >
+              <Form.Label>Book Title</Form.Label>
+              <Form.Control type="text" placeholder="title" onChange={this.handleTitle} />
+              <Form.Label>Book Description</Form.Label>
+              <Form.Control type="text" placeholder="description" onChange={this.handleDescription} />
+              <Form.Label>Book Status</Form.Label>
+              <Form.Control type="text" placeholder="Enter Book Status" onChange={this.handleStatus} />
+              <Form.Label>Contact Email</Form.Label>
+              <Form.Control type="text" placeholder="Enter Contact Email" onChange={this.handleEmail} />
+              <br />
+              <Button variant="dark" value="create" type="submit" >Submit</Button>
+            </Form.Group>
+          </Form>
+          <br />
+
+          {
+            this.state.books.map(book => {
+              return <BestBooks title={book.title}
+                description={book.description}
+                status={book.status} email={book.email}
+                id={book._id}
+                handleDelete={this.handleDelete}
+                handleUpdate={this.handleUpdate} />
+            })
+
+          }
+
+
           <Footer />
         </Router>
       </>
+
+
     )
   }
 }
+
 
 export default App;
--- src/BestBooks.js
+++ src/BestBooks.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import axios from 'axios';
-import { Carousel } from 'react-bootstrap';
+import { Button,Form} from 'react-bootstrap';
 
 
 class BestBooks extends React.Component {
@@ -8,93 +8,51 @@ class BestBooks extends React.Component {
     super(props);
     this.state = {
       books: [],
-      showData: false
+      showData: false,
+        id: " ",
+        title: " ",
+        description: " ",
+        status: " ",
+        email: " ",
+      
     }
   }
-  componentDidMount = async () => {
-    axios.get(`${process.env.REACT_APP_BACKEND_SERVER}/get-data`).then(response => {
-      this.setState({
-        books: response.data,
-        showData: true
-
-      });
-      console.log(response.data);
-    })
-
-  };
-
+  
+  
+  
 
   /* TODO: Make a GET request to your API to fetch books for the logged in user  */
-
   render() {
-
     /* TODO: render user's books in a Carousel */
-
     return (
-     
-
       <>
-      
-        <h1>My Essential Lifelong Learning &amp; Formation Shelf</h1>
+  
+        {/* <h1>My Essential Lifelong Learning Formation Shelf</h1>
         <h2>Books By Author</h2>,
+        {this.state.books.length ? (       
+          this.state.showData &&
+          this.state.books.map(post => {
+            return (
+              <> */}
+              <br />
+             <h1>{this.props.title} </h1> 
+              <br />
+              <p><h3>Description:</h3>{this.props.description}</p>
+              <p><h3>Status:</h3>{this.props.status}</p>
+              <p><h3>Email:</h3>{this.props.email}</p>
+              <p><h3>Id:</h3>{this.props.id}</p>
+              <br />
+              <Button onclick={()=>{this.props.handleDelete(this.props.id)}} variant="dark">Delete</Button> 
+              <br />
+              </>
+        //     )
+        //   })
+        // ) : (
+        //   <h3>No Books Found :(</h3>
+        // )}
 
-        {this.state.books.length ? (
-        //  <p>Book Carousel coming soon</p>,
-        
-         this.state.showData &&
-           this.state.books.map(post => {
-             return (
-               <>
- 
-                 <h3>Author: {post.name} </h3>
-                 <br />
-                 {post.books.map(book => {
- 
-                   return (
-                  //   <Carousel>
-                  //   <Carousel.Item>
-                  //     <img
-                  //       className="d-block w-100"
-                  //       src="https://cdn.lifehack.org/wp-content/uploads/2015/03/50-anniversary-cover1.jpg"
-                  //       alt="First slide"
-                  //     />
-                  //     <Carousel.Caption>
-                  //       <h3>{book.title}</h3>
-                  //       <p>{book.description}</p>
-                  //     </Carousel.Caption>
-                  //   </Carousel.Item>
-                  // </Carousel>
-
-                   <h3>Title:<br /> {book.title}
-                   <br />
-                   <br />
-                   Description:<br /> {book.description}
-                   <br />
-                   <br />
-                   Status:<br /> {book.status}
-                   <br /><br />
-                   Email:<br /> {book.email}
-                   <br /><br />
-                   <br /><br />
-                   </h3>
-                   
-                           
-                   )
- 
-                 })}
-               </>
-             )
- 
- 
-           })
-         
-        ) : (
-          <h3>No Books Found :(</h3>
-        )} 
-       
-      </>
+      
     )
   }
 }
-
 export default BestBooks;
