ef4f8c4ed3ec44a0ad4e363d37d87f5368541b40
SanaIshaqat <sanaishaqat@gmail.com>
2021-09-22 18:21:26 +0300

    lab14

--- src/Header.js
+++ src/Header.js
@@ -1,7 +1,10 @@
 import React from 'react';
-import { Navbar, NavItem } from 'react-bootstrap';
+import { Navbar, NavItem, Button } from 'react-bootstrap';
 import { Link } from "react-router-dom";
 // import './header.css';
+import LoginButton from './components/LoginButton';
+import LogoutButton from './components/LogoutButton';
+import BestBooks from './BestBooks';
 
 class Header extends React.Component {
   render() {
@@ -9,8 +12,17 @@ class Header extends React.Component {
       <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
         <Navbar.Brand>My Favorite Books</Navbar.Brand>
         <NavItem><Link to="/" className="nav-link">Home</Link></NavItem>
-        {/* TODO: if the user is logged in, render a navigation link to profile page */}
-        {/* TODO: if the user is logged in, render the `LogoutButton` */}
+          
+         
+          <>
+          <LogoutButton/>
+          
+          {/* <BestBooks/> */}
+        
+          </>
+          <LoginButton/>
+         
+     
       </Navbar>
     )
   }
